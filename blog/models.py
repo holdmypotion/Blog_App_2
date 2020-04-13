29b18b148fd971fd70a76c32ee1b38b20987d76c
Elijah <rahulsharma240301@gmail.com>
2020-04-13 21:53:42 +0530

    Added ability to make posts from the admin interface

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Post(models.Model):
+    """Post Model"""
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        """Sets the post to the time of publishing"""
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
