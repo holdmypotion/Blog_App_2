4590d29f2eb233f7f71005f796a4114d9bf9d550
Elijah <rahulsharma240301@gmail.com>
2020-04-13 17:21:16 +0530

    Initial commit

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!d:\web develpment\backend\profitable programmer\pp_blog_app\blog_app_2\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
